736639e9e593b124b50d19d4796d7a082bb8d964
Анастасия Бронина <a.o.bronina@tbank.ru>
2025-10-13 13:53:15 +0300

    feat: base structure

--- main.py
+++
@@ -1,9 +0,0 @@
-def sum_three_numbers(a: int, b: int, c: int):
-	return a + b + c;
-
-def main():
-	a, b, c = map(int, input().split())
-	print(sum_three_numbers(a, b, c))
-
-if __name__ == '__main__':
-	main()
---
+++ my_project/__init__.py
@@ -0,0 +1,1 @@
+from my_project import config  # noqa: F401
